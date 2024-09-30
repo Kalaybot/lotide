5ad59c5ed01663ddd4ff6a47eabbe716f1c3e974
Kyle Bautista <[kylealexanderbautista09@gmail.com]>
2024-09-30 14:22:25 -0400

    added new test case

--- tail.js
+++ tail.js
@@ -8,7 +8,10 @@ const assertEqual = function(actual, expected) {
 const tail = function(arr) {
   return arr.slice(1);
 };
-
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); 
+assertEqual(result[0], "Lighthouse"); 
+assertEqual(result[1], "Labs"); 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
+tail(words);
 assertEqual(words.length, 3);
