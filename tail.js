091c2654c328f52bdd6144f34f89171e6af7cc2c
Kyle Bautista <[kylealexanderbautista09@gmail.com]>
2024-09-30 14:25:48 -0400

    new test case with empty array works

--- tail.js
+++ tail.js
@@ -9,9 +9,11 @@ const tail = function(arr) {
   return arr.slice(1);
 };
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); 
-assertEqual(result[0], "Lighthouse"); 
-assertEqual(result[1], "Labs"); 
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words);
 assertEqual(words.length, 3);
+assertEqual(tail(["test"]).length, 0);
+assertEqual(tail([]).length, 0);
