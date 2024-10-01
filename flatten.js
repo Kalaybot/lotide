540136472112b9b35b670d4fb249eb1f60e9e296
Kyle Bautista <[kylealexanderbautista09@gmail.com]>
2024-10-01 18:50:33 -0400

    flatten function works

---
+++ flatten.js
@@ -0,0 +1,34 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`❌ Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+const flatten = function(array) {
+  let result = [];
+
+  for (let element of array) {
+    if (Array.isArray(element)) {
+      for (let innerElement of element) {
+        result.push(innerElement);
+      }
+    } else {
+      result.push(element);
+    }
+  }
+  return result;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
