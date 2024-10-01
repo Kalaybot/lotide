7d0ce4fa02fa1d3385043da4d5e98ff42c756f86
Kyle Bautista <[kylealexanderbautista09@gmail.com]>
2024-10-01 17:40:32 -0400

    without function works as expected

---
+++ without.js
@@ -0,0 +1,41 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`❌ Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+const without = function(source, itemsToRemove) {
+  const results = [];
+
+  for (let i = 0; i < source.length; i++) {
+    if (itemsToRemove.indexOf(source[i]) === -1) {
+      results.push(source[i]);
+    }
+  }
+  return results;
+};
+
+// Let's define some test cases
+assertArraysEqual(without(["hello", "world", "lighthouse"], "world"), ["hello", "lighthouse"]);
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+
+// Test if array is unchanged after calling without
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]);
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+
+console.log("All tests passed!");
