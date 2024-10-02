0eee0794a37efbc9e314978102302da851b490ae
Kyle Bautista <[kylealexanderbautista09@gmail.com]>
2024-10-02 12:26:46 -0400

    counts letters and made use of assertEqual function

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const countLetters = function(str) {
+  let results = {};
+
+  for (const count of str) {
+    if (count !== ' ') {
+      if (results[count]) {
+        results[count] += 1;
+      } else {
+        results[count] = 1;
+      }
+    }
+  }
+  return results;
+};
+const word = countLetters("lighthouse is in the house");
+console.log(word);
+
+assertEqual(word['l'], 1);
+assertEqual(word[' '], undefined);
