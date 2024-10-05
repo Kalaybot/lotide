6bdded9460c430ae961c88799b6f781b1c478d72
Kyle Bautista <[kylealexanderbautista09@gmail.com]>
2024-10-05 18:17:30 -0400

    function works as expected and checks if values are array and returns false

---
+++ eqObjects.js
@@ -0,0 +1,39 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const eqObjects = function(obj1, obj2) {
+  const key1 = Object.keys(obj1);
+  const key2 = Object.keys(obj2);
+  if (key1.length !== key2.length) {
+    return false;
+  }
+  for (const key of key1) {
+    if (obj1[key] !== obj2[key]) {
+      return false;
+    } else if (Array.isArray(obj1[key] && Array.isArray(obj2[key]))) {
+      if (!eqObjects(obj1[key], obj2[key])) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+const house1 = {
+  color: ['brown', 'white', 'grey'],
+  hasRoof: true,
+};
+const house2 = {
+  color: ['grey', 'brown', 'white'],
+  hasRoof: true,
+};
+console.log(eqObjects(house1, house2));
+assertEqual(eqObjects(house1, house2), false);
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+console.log(eqObjects(shirtObject, anotherShirtObject));
+assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
