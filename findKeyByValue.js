741150c08094c65aa316a4cf09a86bb11f51ffd1
Kyle Bautista <[kylealexanderbautista09@gmail.com]>
2024-10-05 15:43:27 -0400

    finds and returns tv shows genre

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const findKeyByValue = function(obj, val) {
+  const keys = Object.keys(obj);
+
+  for (const key of keys) {
+    if (obj[key] === val) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"));
+console.log(findKeyByValue(bestTVShowsByGenre, "HTGAWM"));
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
