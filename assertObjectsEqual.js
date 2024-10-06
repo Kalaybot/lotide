5524c7df6a6779d5fb43ab17acd0b654d592ce80
Kyle Bautista <[kylealexanderbautista09@gmail.com]>
2024-10-06 13:45:10 -0400

    function seems to work although Im not sure if i need to add the eqArrays function as well

---
+++ assertObjectsEqual.js
@@ -0,0 +1,38 @@
+const eqObjects = function(obj1, obj2) {
+  const key1 = Object.keys(obj1);
+  const key2 = Object.keys(obj2);
+  if (key1.length !== key2.length) {
+    return false;
+  }
+  for (const key of key1) {
+    if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
+      if (!eqArrays(obj1[key].sort(), obj2[key].sort())) {
+        return false;
+      }
+    } else if (obj1[key] !== obj2[key]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require("util").inspect;
+
+  if (eqObjects(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+const carObject = {
+  type: "sedan",
+  transmission: "manual",
+  color: "white"
+};
+const carObjectAgain = {
+  type: "sedan",
+  transmission: "manual",
+  color: "black",
+};
+assertObjectsEqual(carObject, carObjectAgain);
