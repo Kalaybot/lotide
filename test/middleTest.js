06b9ea8477c4c296502eb9a53928813bb65fc7e5
Kyle Bautista <[kylealexanderbautista09@gmail.com]>
2024-10-19 18:18:07 -0400

    removed assertEqual and use chai and mocha instead

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,15 @@
 const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual")
+const assert = require("chai").assert;
 
-assertArraysEqual(middle(["hello", "world", "lighthouse"]), ["world"]); // return world
-assertArraysEqual(middle(['abcde']), []); // reurn empty arrays
-assertArraysEqual(middle([40, 45, 50, 52, 53]), [50]); // return 50
-assertArraysEqual(middle([4, 8, 12, 16, 20, 24]), [12, 16]); // return 12, 16
+
+describe("#middle", () => {
+  it("returns ['world'] for ['hello', 'world', 'lighthouse']" , () => {
+    assert.deepEqual(middle(['hello', 'world', 'lighthouse']), ['world']);
+  });
+  it("returns [] for [abcde]", () => {
+    assert.deepEqual(middle(['abcde']), []);
+  });
+  it("returns [12, 16] for [4, 8, 12, 16, 20, 24]", () => {
+    assert.deepEqual(middle([4, 8, 12, 16, 20, 24]), [12, 16]);
+  });
+});
