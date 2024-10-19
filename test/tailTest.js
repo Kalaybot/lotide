698a7731c7bc7ab8145a13ff77dfda8fa86a597b
Kyle Bautista <[kylealexanderbautista09@gmail.com]>
2024-10-19 19:06:03 -0400

    implemented chai and mocha

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,13 @@
 const tail = require("../tail");
-const assertEqual = require("../assertEqual");
+const { assert } = require("chai");
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result.length, 2);
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-assertEqual(tail(["test"]).length, 0);
-assertEqual(tail([]).length, 0);
+
+describe("Tail Function Tests", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    const hello = ['Hello', 'Lighthouse', 'Labs'];
+    assert.deepEqual(tail(hello), ['Lighthouse', 'Labs']);
+  });
+  it("returns [] for an empty array", () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
