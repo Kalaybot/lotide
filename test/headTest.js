0668301606c726f263631b793c782f29de356d2d
Kyle Bautista <[kylealexanderbautista09@gmail.com]>
2024-10-19 17:53:09 -0400

    corrected mispelled string

--- test/headTest.js
+++ test/headTest.js
@@ -10,7 +10,7 @@ describe("#head", () =>  {
   it("returns '5' for ['5']", () => {
     assert.strictEqual(head(['5']), '5');
   });
-  it("retuns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
     assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
   });
 });
